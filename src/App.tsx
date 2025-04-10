2eb7f8d93338093e13fbe7297abbf38f8d8b7631
CristianCabreraObreque <cristian.cabrera@m2msoftware.cl>
2025-04-10 16:54:42 -0400

    logo

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ function App() {
         <div className="container mx-auto px-4 py-4">
           <div className="flex items-center justify-between">
             <div className="flex items-center">
-              <img src="/logo.jpeg" alt="PLS" className="h-12" />
+              <img src="/logo.png" alt="PLS" className="h-12" />
             </div>
 
             {/* Desktop Menu */}
